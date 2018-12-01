ed18b6ba3c8807b2d76a37d2fd4248d1ec843c33
oh29oh29 <hyukjae0529@naver.com>
2018-12-01 13:32:13 +0900

    init

---
+++ src/main/java/pe/oh29oh29/develogm/DevelogMApplication.java
@@ -0,0 +1,12 @@
+package pe.oh29oh29.develogm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelogMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelogMApplication.class, args);
+    }
+}
