63a3e1d8a2f836519c5307805ef26c72f9dd8091
oh29oh29 <hyukjae89@gmail.com>
2018-12-26 23:51:49 +0900

    회원가입 필드 유효성 검사 로직 추가

--- src/main/resources/static/js/member/signUp.js
+++ src/main/resources/static/js/member/signUp.js
@@ -1,7 +1,26 @@
 (function() {
+    'use strict';
+
+    var validationFlag = {
+        'isMatch' : true
+    }
 
     $('#idCheckBtn').on('click', function() {
 
+    });
+
+    $('#passwordConfirm').on('blur', function() {
+        var $this = $(this);
+
+        if (validationFlag.isMatch || $this.val() !== $('#password').val()) {
+            $this.next().text("입력한 비밀번호와 일치하지 않습니다.");
+            $this.next().show();
+            validationFlag.isMatch = false;
+        } else if (!validationFlag.isMatch || $this.val() === $('#password').val()) {
+            $this.next().empty();
+            $this.next().hide();
+            validationFlag.isMatch = true;
+        }
     });
 
 })();
---
+++ src/main/resources/static/js/member/validation.js
@@ -0,0 +1,29 @@
+(function() {
+    'use strict';
+
+    /*
+    * TODO: 필수값 확인
+    */
+    function checkRequiredValue(value) {
+        if (value === undefined || value === null || value === "") {
+            return false;
+        }
+
+        return true;
+    };
+
+    /*
+    * TODO: 영문 확인
+    */
+    function checkOnlyEnglishValue(value) {
+
+    };
+
+    /*
+    * TODO: 최소 입력 제한 확인
+    */
+    function checkMinimumInputValue(value, min) {
+
+    }
+
+})();
