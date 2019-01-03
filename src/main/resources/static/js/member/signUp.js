c4e4122950d34161f533c20d37f779e955aaeb03
oh29oh29 <hyukjae89@gmail.com>
2019-01-03 21:53:41 +0900

    회원가입 필드 유효성 검사 로직 추가 및 수정

--- src/main/resources/static/js/member/signUp.js
+++ src/main/resources/static/js/member/signUp.js
@@ -33,12 +33,12 @@
         var $this = $(this);
 
         if (!(validationFlag.id.isNotEmpty = validator.checkRequiredValue($this))) {
-            $this.siblings('.validation-msg').text("필수값 입니다.").show();
+            showMessage($this.attr('id'), '필수값 입니다.');
             return;
         }
 
         if (!(validationFlag.id.isOnlyAlphabatAndNumber = validator.checkOnlyAlphabatAndNumberValue($this))) {
-            $this.siblings('.validation-msg').text("아이디는 소문자 또는 소문자와 숫자 조합만 가능합니다.").show();
+            showMessage($this.attr('id'), '아이디는 소문자 또는 소문자와 숫자 조합만 가능합니다.');
             return;
         }
 
@@ -48,7 +48,7 @@
             $('#idCheckBtn').addClass('ready-sub-btn').removeClass('failure-sub-btn success-sub-btn');
         }
 
-        $this.siblings('.validation-msg').empty().hide();
+        hideMessage($this.attr('id'));
     });
 
     $('#idCheckBtn').on('click', function() {
@@ -65,11 +65,11 @@
         }).done(function(response) {
 
             if (response.code === responseCode.EXIST_ID) {
-                $this.next().text("이미 존재하는 아이디 입니다.").show();
+                showMessage($this.attr('id'), '이미 존재하는 아이디 입니다.');
                 $this.addClass('failure-sub-btn').removeClass('ready-sub-btn success-sub-btn');
                 validationFlag.id.doesNotExist = false;
             } else if (response.code === responseCode.NOT_EXIST_ID) {
-                $this.next().empty().hide();
+                hideMessage($this.attr('id'));
                 $this.addClass('success-sub-btn').removeClass('ready-sub-btn failure-sub-btn');
                 validationFlag.id.doesNotExist = true;
             }
@@ -85,83 +85,144 @@
         var $this = $(this);
 
         if (!(validationFlag.password.isNotEmpty = validator.checkRequiredValue($this))) {
-            $this.next().text("필수값 입니다.").show();
+            showMessage($this.attr('id'), '필수값 입니다.');
             return;
         }
 
         if (!(validationFlag.password.isMoreThan8 = validator.checkMinimumInputValue($this, 8))) {
-            $this.next().text("비밀번호는 8자 이상입니다.").show();
+            showMessage($this.attr('id'), '비밀번호는 8자 이상입니다.');
             return;
         }
 
-        $this.next().empty().hide();
+        hideMessage($this.attr('id'));
     });
 
     $('#passwordConfirm').on('blur', function() {
         var $this = $(this);
 
         if (!(validationFlag.password.isMatch = validator.checkSameValue($this, $('#password')))) {
-            $this.next().text("입력한 비밀번호와 일치하지 않습니다.").show();
+            showMessage($this.attr('id'), '입력한 비밀번호와 일치하지 않습니다.');
             return;
         }
 
-        $this.next().empty().hide();
+        hideMessage($this.attr('id'));
     });
 
     $('#name').on('blur', function() {
         var $this = $(this);
 
         if (!(validationFlag.name.isNotEmpty = validator.checkRequiredValue($this))) {
-            $this.next().text("필수값 입니다.").show();
+            showMessage($this.attr('id'), '필수값 입니다.');
             return;
         }
 
-        $this.next().empty().hide();
+        hideMessage($this.attr('id'));
     });
 
     $('#email').on('blur', function() {
         var $this = $(this);
 
         if (!(validationFlag.email.isNotEmpty = validator.checkRequiredValue($this))) {
-            $this.next().text("필수값 입니다.").show();
+            showMessage($this.attr('id'), '필수값 입니다.');
             return;
         }
 
         if (!(validationFlag.email.isEmailFormat = validator.checkEmailValue($this))) {
-            $this.next().text("이메일 형식이 아닙니다.").show();
+            showMessage($this.attr('id'), '이메일 형식이 아닙니다.');
             return;
         }
 
-        $this.next().empty().hide();
+        hideMessage($this.attr('id'));
     });
 
     $('#passwordQuestion').on('blur', function() {
         var $this = $(this);
 
         if (!(validationFlag.passwordQuestion.isNotEmpty = validator.checkRequiredValue($this))) {
-            $this.next().text("필수값 입니다.").show();
+            showMessage($this.attr('id'), '필수값 입니다.');
             return;
         }
 
-        $this.next().empty().hide();
+        hideMessage($this.attr('id'));
     });
 
     $('#passwordAnswer').on('blur', function() {
         var $this = $(this);
 
         if (!(validationFlag.passwordAnswer.isNotEmpty = validator.checkRequiredValue($this))) {
-            $this.next().text("필수값 입니다.").show();
+            showMessage($this.attr('id'), '필수값 입니다.');
             return;
         }
 
-        $this.next().empty().hide();
+        hideMessage($this.attr('id'));
     });
 
     $('#signUpForm').on('submit', function() {
-        if (!validationFlag.id.doesNotExist) {
-            $('#id').siblings('.validation-msg').text("중복 확인이 필요합니다.").show();
+
+        if (!validationFlag.id.isNotEmpty) {
+            showMessage('id', '필수값 입니다.');
             return false;
         }
+
+        if (!validationFlag.id.isOnlyAlphabatAndNumber) {
+            showMessage('id', '아이디는 소문자 또는 소문자와 숫자 조합만 가능합니다.');
+            return false;
+        }
+
+        if (!validationFlag.id.doesNotExist) {
+            showMessage('id', '중복 확인이 필요합니다.');
+            return false;
+        }
+
+        if (!validationFlag.password.isNotEmpty) {
+            showMessage('password', '필수값 입니다.');
+            return false;
+        }
+
+        if (!validationFlag.password.isMoreThan8) {
+            showMessage('password', '비밀번호는 8자 이상입니다.');
+            return false;
+        }
+
+        if (!validationFlag.password.isMatch) {
+            showMessage('passwordConfirm', '입력한 비밀번호와 일치하지 않습니다.');
+            return false;
+        }
+
+        if (!validationFlag.name.isNotEmpty) {
+            showMessage('name', '필수값 입니다.');
+            return false;
+        }
+
+        if (!validationFlag.email.isNotEmpty) {
+            showMessage('email', '필수값 입니다.');
+            return false;
+        }
+
+        if (!validationFlag.email.isEmailFormat) {
+            showMessage('email', '이메일 형식이 아닙니다.');
+            return false;
+        }
+
+        if (!validationFlag.passwordQuestion.isNotEmpty) {
+            showMessage('passwordQuestion', '필수값 입니다.');
+            return false;
+        }
+
+        if (!validationFlag.passwordAnswer.isNotEmpty) {
+            showMessage('passwordAnswer', '필수값 입니다.');
+            return false;
+        }
+
+        return true;
     });
+
+    var showMessage = function(domId, message) {
+        $('#' + domId).siblings('.validation-msg').text(message).show();
+    };
+
+    var hideMessage = function(domId) {
+        $('#' + domId).siblings('.validation-msg').empty().hide();
+    };
 
 })();
