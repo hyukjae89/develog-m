e4782ed53f29f8a1ea37233f43f8623e63806f5f
oh29oh29 <hyukjae89@gmail.com>
2019-01-10 23:17:17 +0900

    관리자 카테고리 Controller 수정

---
+++ src/main/java/pe/oh29oh29/develogm/controller/admin/CategoryController.java
@@ -0,0 +1,40 @@
+package pe.oh29oh29.develogm.controller.admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pe.oh29oh29.develogm.model.Category;
+import pe.oh29oh29.develogm.service.admin.CategoryService;
+
+@Controller
+@RequestMapping("/admin/categories")
+public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
+
+    @GetMapping("")
+    public String categoryManagementView(Model model) {
+        model.addAttribute("categories", categoryService.getCategories());
+        return "html/admin/menu/categories";
+    }
+
+    @PostMapping("")
+    public String addCategory(Category category) {
+        categoryService.addCategory(category);
+        return "redirect:/admin/cateogires";
+    }
+
+    @DeleteMapping("")
+    public String deleteCategory(String id) {
+        categoryService.deleteCategory(id);
+        return "redirect:/admin/cateogires";
+    }
+
+    @PatchMapping("")
+    public String updateCategory(Category category) {
+        categoryService.updateCategory(category);
+        return "redirect:/admin/cateogires";
+    }
+}
