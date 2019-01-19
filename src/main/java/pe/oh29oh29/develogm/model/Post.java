d0b5ea62a1e77b5f92474e6f31598369db89fc63
oh29oh29 <hyukjae89@gmail.com>
2019-01-19 11:25:09 +0900

    포스트 엔티티 추가

---
+++ src/main/java/pe/oh29oh29/develogm/model/Post.java
@@ -0,0 +1,49 @@
+package pe.oh29oh29.develogm.model;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "posts")
+@Data
+public class Post implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+        name = "UUID",
+        strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private String id;
+
+    @Column
+    private String categoryId;
+
+    @Column
+    private String memberId;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column
+    private String contents;
+
+    @Column
+    private String regDate;
+
+    @Column
+    private String lastUpdateDate;
+
+    @Column
+    private int views;
+
+    @Column
+    private boolean isPrivate;
+
+}
